96664a98f30e9a6178a62b2e1a83d920c1b4f0fa
ammaralawadatezdan <148220380+ammaralawadatezdan@users.noreply.github.com>
2024-01-20 11:36:10 +0300

    New Changes

--- src/assets/js/product.js
+++ src/assets/js/product.js
@@ -6,6 +6,7 @@ import { zoom } from './partials/image-zoom';
 
 class Product extends BasePage {
     onReady() {
+
         this.catblock()
         app.watchElements({
             totalPrice: '.total-price',
@@ -55,42 +56,9 @@ class Product extends BasePage {
 
     async catblock()
     {
-// Assume salla.api.request('component/list', {params: {paths:['home.main-links']}}) returns a Promise
-salla.api.request('component/list', {params: {paths:['home.main-links']}})
-.then((res) => {
-    // Assuming res contains a data array with at least one item
-    const dataArray = res.data;
 
-    // Access the div element by its id for string_1
-    const outputString1Div = document.getElementById('outputString1');
 
-    // Get the current URL
-    const currentURL = window.location.href;
 
-    // Filter the array based on the condition
-    const filteredArray = dataArray.filter((item) =>
-        item &&
-        item.item_collection &&
-        item.item_collection.length > 0 &&
-        item.component &&
-        item.component.ar &&
-        item.component.ar.title === currentURL
-    );
-
-    // If filteredArray is not empty and the first item in item_collection has the string_1 property, display it
-    if (filteredArray.length > 0 && filteredArray[0].item_collection[0] && filteredArray[0].item_collection[0].string_1) {
-        const string1Value = filteredArray[0].item_collection[0].string_1;
-
-        // Set the text content of the outputString1 div
-        outputString1Div.textContent = string1Value;
-    } else {
-        // If filteredArray is empty or the structure is different, you might want to hide the corresponding div or handle it in a way that fits your design.
-        outputString1Div.style.display = 'none';
-    }
-})
-.catch((error) => {
-    console.error('Error fetching data:', error);
-});
     
     }
     
